a1177f8dd25646f7b99c930ba4e6c4b749447e63
adunStudio <adun221@naver.com>
2016-10-26 19:34:16 +0900

    쿠키 만료 일자

--- public/js/adundocs_ThemeCtrl.js
+++ public/js/adundocs_ThemeCtrl.js
@@ -3,12 +3,17 @@ AdunDocs.controller('ThemeCtrl', ['$scope', '$cookieStore', function ThemeCtrl($
     var WHITE = '/css/style_white.css';
     var BLACK = '/css/style_black.css';
 
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 3);
+
     $scope.initTheme = function() {
         var theme = $cookieStore.get('theme') || WHITE;
         $scope.setTheme(theme);
 
         $('#theme').attr('href', $scope.theme);
     };
+
+
 
     $scope.initTheme();
 
@@ -17,7 +22,7 @@ AdunDocs.controller('ThemeCtrl', ['$scope', '$cookieStore', function ThemeCtrl($
         var current = $('#theme').attr('href');
         var theme = (current == WHITE) ? BLACK : WHITE;
 
-        $cookieStore.put('theme', theme);
+        $cookieStore.put('theme', theme, {'expires': expireDate});
         $scope.setTheme(theme);
 
         $('#theme').attr('href', $scope.theme);
